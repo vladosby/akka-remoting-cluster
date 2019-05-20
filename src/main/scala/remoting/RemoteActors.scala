50912a0d4cbac333185be06e86ec62ce89e28ca6
vladosby <vladislavn.kievski@gmail.com>
2019-05-20 21:37:26 +0300

    Separate to 2 app

--- src/main/scala/remoting/RemoteActors.scala
+++ src/main/scala/remoting/RemoteActors.scala
@@ -1,15 +1,32 @@
 package remoting
 
 import akka.actor.{ActorSystem, Props}
+import akka.util.Timeout
 import com.typesafe.config.ConfigFactory
+
+import scala.concurrent.duration._
+import scala.util.{Failure, Success}
 
 object RemoteActors extends App {
   val localSystem = ActorSystem("LocalSystem", ConfigFactory.load("remoting/remoting.conf"))
-  val remoteSystem = ActorSystem("RemoteSystem", ConfigFactory.load("remoting/remoting.conf").getConfig("remoteSystem"))
-
   val localSimpleActor = localSystem.actorOf(Props[SimpleActor], "localSimpleActor")
-  val remoteSimpleActor = remoteSystem.actorOf(Props[SimpleActor], "remoteSimpleActor")
 
   localSimpleActor ! "hello, local actor!"
+
+  val remoteActorSelection = localSystem.actorSelection("akka://RemoteSystem@localhost:2552/user/remoteSimpleActor")
+  remoteActorSelection ! "hello form local JVm"
+
+  import localSystem.dispatcher
+  implicit val timeout = Timeout(3 seconds)
+  val remoteActorSelectionFuture = remoteActorSelection.resolveOne()
+  remoteActorSelectionFuture.onComplete{
+    case Success(actorRef) => actorRef ! "I've resolve you in the future!"
+    case Failure(ex) => println(s"Failure to resolve a future $ex")
+  }
+}
+
+object RemoteActorJVM extends App {
+  val remoteSystem = ActorSystem("RemoteSystem", ConfigFactory.load("remoting/remoting.conf").getConfig("remoteSystem"))
+  val remoteSimpleActor = remoteSystem.actorOf(Props[SimpleActor], "remoteSimpleActor")
   remoteSimpleActor ! "hello, remote actor!"
 }
--- src/main/scala/remoting/SimpleActor.scala
+++ src/main/scala/remoting/SimpleActor.scala
@@ -4,6 +4,7 @@ import akka.actor.{Actor, ActorLogging}
 
 class SimpleActor extends Actor with ActorLogging{
   override def receive ={
-    case m => log.info(s"Received0 $m from ${sender()}")
+    case m =>
+      log.info(s"Received0 $m from ${sender()}")
   }
 }
