e92c5778d5b281e68640b98344c7e88c8ca97844
vladosby <vladislavn.kievski@gmail.com>
2019-05-22 21:30:59 +0300

    Simple example of remoting deployment

---
+++ src/main/scala/remoting/DeployingActorsRemotely.scala
@@ -0,0 +1,16 @@
+package part2_remoting
+
+import akka.actor.{ActorSystem, Props}
+import com.typesafe.config.ConfigFactory
+import remoting.SimpleActor
+
+object DeployingActorsRemotely_LocalApp extends App {
+  val system = ActorSystem("LocalActorSystem", ConfigFactory.load("remoting/deployingActorsRemotely.conf").getConfig("localApp"))
+
+  val simpleActor = system.actorOf(Props[SimpleActor], "remoteActor") // /user/remoteActor
+  simpleActor ! "hello, remote actor!"
+}
+
+object DeployingActorsRemotely_RemoteApp extends App {
+  val system = ActorSystem("RemoteActorSystem", ConfigFactory.load("remoting/deployingActorsRemotely.conf").getConfig("remoteApp"))
+}
